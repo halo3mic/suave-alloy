0f5921636eb5eb207c00289bebf444b17c9a8663
halo3mic <46010359+halo3mic@users.noreply.github.com>
2024-04-16 00:41:19 +0100

    SuaveSignerProvider trait

--- crates/network/src/provider.rs
+++ crates/network/src/provider.rs
@@ -1,30 +1,31 @@
+use std::str::FromStr;
+
 use alloy::{
-    providers::{Provider, ProviderBuilder, RootProvider},
+    providers::{Provider, ProviderBuilder, RootProvider, layers::SignerProvider},
     transports::{http::Http, TransportResult},
+    rpc::client::ClientRef,
     primitives::Address,
 };
 use reqwest::Client as ReqwestClient;
 use super::network::SuaveNetwork;
+use super::signer::SuaveSigner;
 
 
+#[derive(Clone)]
 pub struct SuaveProvider {
     root_provider: RootProvider<Http<ReqwestClient>, SuaveNetwork>,
 }
 
 impl SuaveProvider {
 
-    pub fn from_url(url: &str) -> Self {
-        let url = url.parse().unwrap();
+    pub fn new(url: url::Url) -> Self {
         let root_provider = ProviderBuilder::<_, SuaveNetwork>::default()
             .on_reqwest_http(url).expect("Failed to root provider for SuaveProvider");
         Self { root_provider }
     }
 
     pub async fn kettle_address(&self) -> TransportResult<Address> {
-        self.client()
-            .request(String::from("eth_kettleAddress"), ())
-            .await
-            .map(|ks: Vec<Address>| ks[0])
+        kettle_address(self.client()).await
     }
 
 }
@@ -37,16 +38,52 @@ impl Provider<Http<ReqwestClient>, SuaveNetwork> for SuaveProvider {
 
 }
 
+impl TryFrom<&str> for SuaveProvider {
+    type Error = url::ParseError;
+
+    fn try_from(url: &str) -> Result<Self, Self::Error> {
+        Ok(SuaveProvider::new(url.parse()?))
+    }
+
+}
+
+impl FromStr for SuaveProvider {
+    type Err = url::ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(SuaveProvider::new(s.parse()?))
+    }
+
+}
+
+pub trait SuaveSignerProvider {
+    fn kettle_address(&self) -> impl std::future::Future<Output = TransportResult<Address>> + Send;
+}
+
+impl SuaveSignerProvider for SignerProvider<Http<ReqwestClient>, SuaveProvider, SuaveSigner, SuaveNetwork> {
+    async fn kettle_address(&self) -> TransportResult<Address> {
+        kettle_address(self.client()).await
+    }
+}
+
+async fn kettle_address<'a>(client: ClientRef<'a ,Http<reqwest::Client>>) -> TransportResult<Address> {
+    client.request(String::from("eth_kettleAddress"), ()).await
+        .map(|ks: Vec<Address>| ks[0])
+}
+
+
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
+    use eyre::Result;
     use super::*;
 
     #[tokio::test]
-    async fn test_suave_provider() {
-        let provider = SuaveProvider::from_url("https://rpc.rigil.suave.flashbots.net");
+    async fn test_suave_provider() -> Result<()> {
+        let provider = SuaveProvider::try_from("https://rpc.rigil.suave.flashbots.net")?;
         let kettle_address = provider.kettle_address().await.unwrap();
         assert_eq!(kettle_address, Address::from_str("0x03493869959c866713c33669ca118e774a30a0e5").unwrap());
+        Ok(())
     }
 
 }
--- examples/submit_ccr.rs
+++ examples/submit_ccr.rs
@@ -1,22 +1,20 @@
 use std::str::FromStr;
-use eyre::Result;
+use eyre::{OptionExt, Result};
 use alloy::{
     primitives::{Address, Bytes, B256, U256}, 
     providers::{Provider, ProviderBuilder}, 
-    rpc::types::eth::TransactionRequest, 
     signers::wallet::LocalWallet,
     network::TransactionBuilder,
 };
 use suave_alloy::{
-    types::{ConfidentialComputeRecord, ConfidentialComputeRequest},
-    network::{SuaveProvider, SuaveSigner},
+    types::ConfidentialComputeRequest,
+    network::{SuaveProvider, SuaveSigner, SuaveSignerProvider},
 };
-
 
 
 #[tokio::main]
 
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     // Args
     let input = Bytes::from_str("0x50723553000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000074554485553445400").unwrap();
     let wallet_address = Address::from_str("0x19E7E376E7C213B7E7e7e46cc70A5dD086DAff2A").unwrap();
@@ -26,12 +24,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let chain_id = 0x1008c45;
     let gas = 0x0f4240;
 
-    // Create SUAVE provider
-    let provider = SuaveProvider::from_url("https://rpc.rigil.suave.flashbots.net");
+    // Create SUAVE signer-provider
+    let rpc_url = "https://rpc.rigil.suave.flashbots.net";
+    let wallet: LocalWallet = "0x1111111111111111111111111111111111111111111111111111111111111111".parse()?;    
+    let provider = ProviderBuilder::default()
+        .signer(SuaveSigner::from(wallet.clone()))
+        .provider(SuaveProvider::try_from(rpc_url)?);
 
     // Get nonce and kettle address
-    let tx_count: u64 = provider.get_transaction_count(wallet_address, None).await.unwrap().to();
-    let kettle = provider.kettle_address().await.unwrap();
+    let tx_count: u64 = provider.get_transaction_count(wallet_address, None).await?.to();
+    let kettle = provider.kettle_address().await?;
 
     // Create a confidential-compute-request 
     let ccr = ConfidentialComputeRequest::default()
@@ -43,19 +45,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_input(input)
         .with_confidential_inputs(cinputs)
         .with_kettle_address(kettle);
-
-    // Create a signer
-    let wallet: LocalWallet = "0x1111111111111111111111111111111111111111111111111111111111111111".parse().unwrap();    
-    let signer = SuaveSigner::from(wallet.clone());
-    let provider = ProviderBuilder::default().signer(signer).provider(provider);
-
-    let result = provider.send_transaction(ccr).await.unwrap();
+    
+    let result = provider.send_transaction(ccr).await?;
     let tx_hash = B256::from_slice(&result.tx_hash().to_vec());
-    let tx_response = provider.get_transaction_by_hash(tx_hash).await.unwrap();
+    let tx_response = provider.get_transaction_by_hash(tx_hash).await?;
 
     println!("{tx_response:#?}");
 
-    let price = U256::try_from_be_slice(&tx_response.confidential_compute_result.to_vec()).unwrap();
+    let price = U256::try_from_be_slice(&tx_response.confidential_compute_result.to_vec())
+        .ok_or_eyre("conf result is not U256")?;
     println!("Price: {:?}", price.wrapping_to::<u128>());
 
     Ok(())
