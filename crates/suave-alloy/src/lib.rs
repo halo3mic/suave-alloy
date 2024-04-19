57a9a07067ced8746dc9e9091f89467fc0dc1ec8
halo3mic <46010359+halo3mic@users.noreply.github.com>
2024-04-19 14:26:21 +0100

    add prelude

--- crates/suave-alloy/src/lib.rs
+++ crates/suave-alloy/src/lib.rs
@@ -1,3 +1,8 @@
 pub use suave_alloy_types as types;
 #[cfg(feature = "network")]
 pub use suave_alloy_network as network;
+pub mod prelude {
+    pub use suave_alloy_types::*;
+    #[cfg(feature = "network")]
+    pub use suave_alloy_network::*;
+}
